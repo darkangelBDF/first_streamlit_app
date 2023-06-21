c186d7cd7acf154919ee60106751ec9ba128fcb1
Debbie Swanepoel <DDippe417@ignitiongroup.co.za>
2023-06-21 09:56:27 +0200

    Update streamlit_app.py
    Lesson 9

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,5 +22,5 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
-fruity_vice_response = ("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruity_vice_response)
+fruityvice_response = requests.get ("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
